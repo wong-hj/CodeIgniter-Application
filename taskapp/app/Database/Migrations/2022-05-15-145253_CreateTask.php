42a7f9b9f0c2078897606add94759e029d03987f
june <hj.wong02@gmail.com>
2022-05-15 23:02:12 +0800

    Create table in database
    Created table in database using codeigniter migration method, manipulate database with database forge.

---
+++ taskapp/app/Database/Migrations/2022-05-15-145253_CreateTask.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTask extends Migration
+{
+    public function up()
+    {
+        //add tables and fields when command run in CMD
+        //command: php spark migrate
+        $this ->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true
+            ],
+            'description' => [
+                'type'           => 'VARCHAR',
+                'constraint'     => 128,
+            ]
+        ]);
+
+        $this ->forge->addPrimaryKey('id');
+        $this->forge->createTable('task');
+    }
+
+    public function down()
+    {
+        //undo actions made when command run in CMD
+        //command: php spark migrate:rollback (rollback most recent changes on database).
+        $this ->forge->dropTable('task');
+    }
+}
