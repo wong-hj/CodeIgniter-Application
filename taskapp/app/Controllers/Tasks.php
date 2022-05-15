c8235910c766a539e302d728159839b3d3dd38fe
june <hj.wong02@gmail.com>
2022-05-15 23:11:43 +0800

    Use model to select table then show all data with controller

--- taskapp/app/Controllers/Tasks.php
+++ taskapp/app/Controllers/Tasks.php
@@ -6,10 +6,13 @@ class Tasks extends BaseController
 {
     public function index()
     {
-        $data = [
-            ["id" => 1, "description" => "First Task"],
-            ["id" => 2, "description" => "Second Task"]
-        ]; 
+        // $data = [
+        //     ["id" => 1, "description" => "First Task"],
+        //     ["id" => 2, "description" => "Second Task"]
+        // ]; 
+
+        $model = new \App\Models\TaskModel;
+        $data = $model->findAll();
 
         return view("Tasks/index.php", ["tasks" => $data]);
     }
